ab746b27d75f49bc3eab65a09699f81f96806f6a
menji40ji26 <420870586@qq.com>
2018-02-27 23:01:40 -0500

    Initial commit

---
+++ Assets/gameControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameControl : MonoBehaviour {
+
+	public Text cashText;
+	public Text fishText;
+	public float cash;
+	public float fish;
+
+	// Use this for initialization
+	void Start () {
+		cash = 100;
+		cashText.text = "Cash: $" + cash;
+		fishText.text = "Fish: " + fish;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		cashText.text = "Cash: $" + cash;
+		fishText.text = "Fish: " + fish;
+	}
+
+	public void buyFish(){
+		if(cash >= 10){
+			cash -= 10;
+			fish ++;
+		}
+
+	}
+}
---
+++ Assets/save.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class save : MonoBehaviour {
+
+	public gameControl gc;
+
+	public void Save(){
+		PlayerPrefs.SetFloat("Cash", gc.cash);
+		PlayerPrefs.SetFloat("Fish", gc.fish);
+	}
+
+	public void Load(){
+		gc.cash = PlayerPrefs.GetFloat("Cash");
+		gc.fish = PlayerPrefs.GetFloat("Fish");
+
+	}
+
+}
